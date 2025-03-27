5362a68731972419f41f31ff89da32342ac30f0d
Zaryab Zubair <zaryabz043@gmail.com>
2025-03-27 10:53:59 +0500

    added custom domain

--- services/generateTokenAndSetCookie.js
+++ services/generateTokenAndSetCookie.js
@@ -11,6 +11,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     sameSite: "None",
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: ".voidia-backend.vercel.app",
   });
   return token;
 };
