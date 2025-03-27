645c7d41214bd7e4a4596a3f9aa026e0762be7fe
Zaryab Zubair <zaryabz043@gmail.com>
2025-03-27 10:36:14 +0500

    update cookie settings

--- services/generateTokenAndSetCookie.js
+++ services/generateTokenAndSetCookie.js
@@ -8,10 +8,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
-
   return token;
 };
